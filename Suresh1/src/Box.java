48e28f5246562a37fdc76b88751d6d6cce68ee54
sureshvaibu <suresh.kethari@gmail.com>
2019-09-18 21:39:48 -0500

    Second commit

---
+++ Suresh1/src/Box.java
@@ -0,0 +1,20 @@
+
+public class Box {
+
+	public static void main(String[] args)
+	{
+		double lenght, width, hight;
+		double cal;
+		
+		lenght = 10;
+		width = 10;
+		hight = 10;
+		
+		cal = lenght*width*hight;
+		System.out.println("Calculate Volume is:"+cal);
+		
+		
+
+	}
+
+}
