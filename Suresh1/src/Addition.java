3e45bbf6b2f4b7bc9fc6f5ed6d77281f39a723c4
sureshvaibu <suresh.kethari@gmail.com>
2019-09-18 21:26:12 -0500

    First Commit

---
+++ Suresh1/src/Addition.java
@@ -0,0 +1,17 @@
+
+public class Addition {
+
+	public static void main(String[] args)  
+	{
+
+		int a = 30;
+		int b = 30;
+		int c;
+		
+		c = a + b;
+		System.out.println("Total num is:"+c);
+				
+
+	}
+
+}
