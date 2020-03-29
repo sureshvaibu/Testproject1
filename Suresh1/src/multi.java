816eb684233966e693f2bcbaf2206b0f19dcd5f6
sureshvaibu <suresh.kethari@gmail.com>
2020-03-29 18:34:56 -0500

    add multiplication class

---
+++ Suresh1/src/multi.java
@@ -0,0 +1,15 @@
+
+public class multi {
+
+	public static void main(String[] args) 
+	{
+		int a = 30;
+		int b = 30;
+		int c;
+		
+		c = a * b;
+		System.out.println("Total multiplication is:"+c);
+
+	}
+
+}
